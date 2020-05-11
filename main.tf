dc0fe10e2116c915a0cd28f402213098b643b64d
SAGAR JADHAV <sagarj.jadhav23@gmail.com>
2020-05-11 23:35:24 +0530

    Added email config

--- main.tf
+++ main.tf
@@ -14,3 +14,31 @@ resource "helm_release" "frontend" {
   depends_on = [helm_release.backend]
 }
 
+variable "mail_port" {
+  type = string
+}
+
+variable "mail_smtp_server" {
+  type = string
+}
+
+variable "mail_login" {
+  type = string
+}
+
+variable "mail_password" {
+  type = string
+}
+
+resource "null_resource" "example1" {
+  provisioner "local-exec" {
+    command = "python3 mail.py $MAIL_PORT $SMTP_SERVER $USERNAME $PASSWORD"
+
+    environment = {
+      MAIL_PORT = "${var.mail_port}"
+      SMTP_SERVER = "${var.mail_smtp_server}"
+      USERNAME = "${var.mail_login}"
+      PASSWORD = "${var.mail_password}"
+    }
+  }
+}
