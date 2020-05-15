38cff782058a2da73a8db3bb09e8fd29afe4a349
SAGAR JADHAV <sagarj.jadhav23@gmail.com>
2020-05-16 00:12:17 +0530

    update

--- main.tf
+++ main.tf
@@ -6,8 +6,20 @@ resource "random_password" "password" {
   length = 4
 }
 
+resource "random_string" "backend_release_name" {
+  length  = 10
+  special = false
+  number  = false
+}
+
+resource "random_string" "frontend_release_name" {
+  length  = 10
+  special = false
+  number  = false
+}
+
 resource "helm_release" "backend" {
-  name  = "backend"
+  name  = random_string.backend_release_name.result
   chart = "./backend"
 
   set {
@@ -15,11 +27,11 @@ resource "helm_release" "backend" {
     value = random_password.password.result
   }
 
-  depends_on = [helm_release.backend]
+  depends_on = [random_password.password, random_string.backend_release_name]
 }
 
 resource "helm_release" "frontend" {
-  name  = "frontend"
+  name  = random_string.frontend_release_name
   chart = "./frontend"
 
   set {
@@ -27,7 +39,7 @@ resource "helm_release" "frontend" {
     value = random_password.password.result
   }
 
-  depends_on = [helm_release.backend]
+  depends_on = [helm_release.backend, random_password.password, random_string.frontend_release_name]
 }
 
 resource "null_resource" "example1" {
@@ -35,10 +47,10 @@ resource "null_resource" "example1" {
     command = "python3 mail.py $MAIL_PORT $SMTP_SERVER $USERNAME $PASSWORD $MYSQL_PASSWORD"
 
     environment = {
-      MAIL_PORT = var.mail_port
-      SMTP_SERVER = var.mail_smtp_server
-      USERNAME = var.mail_login
-      PASSWORD = var.mail_password
+      MAIL_PORT      = var.mail_port
+      SMTP_SERVER    = var.mail_smtp_server
+      USERNAME       = var.mail_login
+      PASSWORD       = var.mail_password
       MYSQL_PASSWORD = random_password.password.result
     }
   }
